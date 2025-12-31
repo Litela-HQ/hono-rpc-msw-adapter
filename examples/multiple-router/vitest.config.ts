00620b7bfc31287cb5b574f77ce7de3c99dad144
d-kimsuon <dev.kimuson@gmail.com>
2025-12-31 19:57:46 +0900

    chore: fix ci

--- examples/multiple-router/vitest.config.ts
+++ examples/multiple-router/vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  resolve: {
-    conditions: ['@hono-rpc-msw-adapter/src'],
-  },
   test: {
     globals: true,
     environment: 'node',
